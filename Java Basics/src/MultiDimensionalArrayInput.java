7a7cf830711f4077e69c7a771067aab58082cddf
Ayushmaan Singh <ayushmaan0313@gmail.com>
2024-12-24 22:55:39 +0530

    Implementation of User Input Multidimentional Array
    Demonstration of  multidimensional array by taking input from users about the size and elements of the array

---
+++ Java Basics/src/MultiDimensionalArrayInput.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class MultiDimensionalArrayInput {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+        System.out.print("Enter the number of columns: ");
+        int columns = scanner.nextInt();
+
+        int[][] array = new int[rows][columns];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print("Element at [" + i + "][" + j + "]: ");
+                array[i][j] = scanner.nextInt();
+            }
+        }
+        System.out.println("The entered 2D array is:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
